1aed1a47954536903fcbe79e922e228e11e4097f
shilpespillai <shilpespillai@users.noreply.github.com>
2026-07-30 16:44:20 +1000

    fix: remove invalid response_format parameter from DALL-E 3 request payload

--- api/generate-image.js
+++ api/generate-image.js
@@ -30,8 +30,7 @@ export default async function handler(req, res) {
         prompt: prompt,
         n: 1,
         size: '1024x1024',
-        quality: 'standard',
-        response_format: 'url'
+        quality: 'standard'
       })
     });
 
